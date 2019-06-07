ba12274864f68ab74cc306e333277c3b825a7ff4
Tim Gates <tim.gates@iress.com>
2019-06-07 16:53:29 +1000

    Fix up install dir

--- app/install.sh
+++ app/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -euxo pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${BASEDIR}"
+
 python -m pip install pipenv
 python -m pipenv install --deploy --system 
 
