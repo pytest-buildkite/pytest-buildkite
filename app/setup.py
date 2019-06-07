ab8f51471e91b3e17dccfc3bdb9a5929125ec73a
Tim Gates <tim.gates@iress.com>
2019-06-07 21:25:34 +1000

    Fix up setuptools packaging options

--- app/setup.py
+++ app/setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name='pyruncompare',
-    version='0.1dev',
+    version='0.1.1dev',
+    author='Tim Gates',
+    author_email='tim.gates@iress.com',
     packages=['pyruncompare',],
     license='GPLv3+',
     long_description=(
@@ -10,4 +12,11 @@ setup(
         ' calls and locals to formats that can be compared'
         ' for execution variations.'
     ),
+    url='https://github.com/pyruncompare-dev/pyruncompare',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
+        "Operating System :: OS Independent",
+    ],
 )
