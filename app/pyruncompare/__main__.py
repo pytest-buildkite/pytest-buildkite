60914c72a2e51387d7ba6c0ff5af2aa3cc571b72
Tim Gates <tim.gates@iress.com>
2019-06-08 12:25:56 +1000

    Add required blank lines

--- app/pyruncompare/__main__.py
+++ app/pyruncompare/__main__.py
@@ -5,11 +5,13 @@ from __future__ import (
     absolute_import, print_function, division, unicode_literals,
 )
 
+
 def main():
     """
     Test
     """
     pass
+
 
 if __name__ == '__main__':
     main()
