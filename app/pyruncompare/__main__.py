6df67076810ac95f70a7810fc062d7f8517a81ce
Tim Gates <tim.gates@iress.com>
2019-06-08 11:56:26 +1000

    Add empty main

---
+++ app/pyruncompare/__main__.py
@@ -0,0 +1,15 @@
+"""
+Module load handler for execution via python -m pyruncompare.
+"""
+from __future__ import (
+    absolute_import, print_function, division, unicode_literals,
+)
+
+def main():
+    """
+    Test
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
