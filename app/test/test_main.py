79d9c04067be33276a07387e7eb04752c1ac3f7b
Tim Gates <tim.gates@iress.com>
2019-06-09 11:03:00 +1000

    Reduce line length

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -5,8 +5,8 @@ Test modules for pymodulenamegoeshere __main__
 
 def test_main():
     """
-    GIVEN the pymodulenamegoeshere.__main__ module entry point WHEN calling main THEN
-    the call executes successfully with a result of `None`
+    GIVEN the pymodulenamegoeshere.__main__ module entry point WHEN calling main
+    THEN the call executes successfully with a result of `None`
     """
     # Setup
     from pymodulenamegoeshere.__main__ import main
