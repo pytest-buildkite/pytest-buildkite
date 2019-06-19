ce9733b358f647d2ec685b6cff6a4ce1ef73ca65
Tim Gates <tim.gates@iress.com>
2019-06-20 09:53:41 +1000

    Build All Python Versions with Pipenv

--- app/install.sh
+++ app/install.sh
@@ -6,6 +6,33 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${BASEDIR}"
 
-python -m pip install pipenv
-python -m pipenv install --deploy --system 
+PYTHONVERS=( 
+    python2.5 \
+    python2.6 \
+    python2.7 \
+    python3.1 \
+    python3.2 \
+    python3.3 \
+    python3.4 \
+    python3.5 \
+    python3.6 \
+    python3.7 \
+    pypy \
+)
 
+apt-get update
+apt-get install -qq -y openssl
+apt-get install -qq -y software-properties-common
+add-apt-repository ppa:deadsnakes
+add-apt-repository ppa:pypy/ppa
+apt-get update
+apt-get install -qq -y "${PYTHONVERS[@]}"
+
+apt-get install -qq -y curl
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+for PYVER in "${PYTHONVERS[@]}" ; do
+  "${PYVER}" get-pip.py
+  "${PYVER}" -m pip install pipenv
+  "${PYVER}" -m pipenv install --deploy --system 
+done
