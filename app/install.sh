792c3251ee10072db80455561e5f756c298a3e82
Tim Gates <tim.gates@iress.com>
2019-06-29 13:46:41 +1000

    Using pipenv-all

--- app/install.sh
+++ app/install.sh
@@ -6,31 +6,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${BASEDIR}"
 
-PYTHONVERS=( 
-    python2.7 \
-    python3.1 \
-    python3.2 \
-    python3.3 \
-    python3.4 \
-    python3.5 \
-    python3.6 \
-    python3.7 \
-    pypy \
-)
-
-apt-get update
-apt-get install -qq -y openssl
-apt-get install -qq -y software-properties-common
-add-apt-repository ppa:deadsnakes
-add-apt-repository ppa:pypy/ppa
-apt-get update
-apt-get install -qq -y "${PYTHONVERS[@]}"
-
-apt-get install -qq -y curl
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-
-for PYVER in "${PYTHONVERS[@]}" ; do
-  "${PYVER}" get-pip.py
-  "${PYVER}" -m pip install pipenv
-  "${PYVER}" -m pipenv install --deploy --system 
+for PYVER in ${PYTHONVERS} ; do
+  cd "${BASEDIR}/pipenv/${PYVER}"
+  "python${PYVER}" -m pipenv install pyspelling #--deploy --system 
 done
