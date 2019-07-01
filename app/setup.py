f7fc2c8ffd9ed0b19cc4bd0d62dc0a63fa738112
Tim Gates <tim.gates@iress.com>
2019-07-01 21:30:09 +1000

    Initial publish

--- app/setup.py
+++ app/setup.py
@@ -1,24 +1,36 @@
 """
-Setuptool Distribution for pymodulenamegoeshere
+Setuptool Distribution for pytest_buildkite
 """
 from setuptools import setup
 
 setup(
-    name='pymodulenamegoeshere',
-    version='0.1.1dev',
+    name='pytest-buildkite',
+    version='0.1.2dev0',
     author='Tim Gates',
     author_email='tim.gates@iress.com',
-    packages=['pymodulenamegoeshere',],
-    license='GPLv3+',
+    packages=['pytest_buildkite',],
+    license='MIT',
     long_description=(
-        'Execute a python module or function and log all'
-        ' calls and locals to formats that can be compared'
-        ' for execution variations.'
+        'Plugin for pytest that automatically publishes coverage and'
+        ' pytest report annotations to Buildkite.'
     ),
-    url='homepageurlgoeshere',
+    url='https://pytest-buildkite.github.io/index.html',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
-        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
     ],
 )
