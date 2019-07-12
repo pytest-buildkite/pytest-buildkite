51e322be09bb1c9dc3edcad9c4f67cf62955c55b
Tim Gates <tim.gates@iress.com>
2019-07-12 22:23:28 +1000

    Fix conflict

--- ci/in_docker/pylint.sh
+++ ci/in_docker/pylint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 set -euxo pipefail
-<<<<<<< HEAD
-PYTHON="${1}"
-TARGET="${2}"
-if ! "${PYTHON}" -m pylint "${2}" ; then
-=======
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR="$( dirname "$( dirname "${THISDIR}" )" )"
@@ -13,7 +8,6 @@ BASEDIR="$( dirname "$( dirname "${THISDIR}" )" )"
 PYTHON="${1}"
 TARGET="${2}"
 if ! "${PYTHON}" -m pylint --rcfile "${BASEDIR}/app/.pylintrc" "${2}" ; then
->>>>>>> d6e2066bc7dfe5a9e7daeb0d7a6cd1ce438b42a5
    echo "Pylint failed on ${2}" >&2
    exit 1
 fi
