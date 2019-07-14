0b95472a8c475c8457757da2b14f26f0b7fe4c48
Tim Gates <tim.gates@iress.com>
2019-07-14 10:57:47 +1000

    Just run pyspelling with latest python

--- app/install.sh
+++ app/install.sh
@@ -8,7 +8,10 @@ cd "${BASEDIR}"
 
 for PYVER in ${PYTHONVERS} ; do
   cd "${BASEDIR}/pip/${PYVER}"
-  "python${PYVER}" -m pip install -r requirements.txt
+  "python${PYVER}" -m pip install -r test_requirements.txt
+  if [ $(wc -l requirements.txt) -gt 0 ] ; then
+      "python${PYVER}" -m pip install -r requirements.txt
+  fi
   # Display installation
   "python${PYVER}" -m pip freeze
 done
--- ci/in_docker/test.sh
+++ ci/in_docker/test.sh
@@ -8,11 +8,14 @@ BASEDIR="$( dirname "$( dirname "${THISDIR}" )" )"
 source ${BASEDIR}/ci/in_docker/prepare.sh
 
 cd "${BASEDIR}/app"
+# Version independant checks
+PYVER=3.7
+"python${PYVER}" -m pyspelling
+# Version dependant checks
 for PYVER in ${PYTHONVERS} ; do
   "python${PYVER}" -m flake8 "${MODULES[@]}"
   "python${PYVER}" -m isort -rc -c --diff "${MODULES[@]}"
   "python${PYVER}" -m bandit -r "${MODULES[@]}"
-  "python${PYVER}" -m pyspelling
   find "${MODULES[@]}" -iname \*.py -print0 | xargs -0 -n 1 "${BASEDIR}/ci/in_docker/pylint.sh" "python${PYVER}"
   "python${PYVER}" -m pytest --cov-config=.coveragerc --cov-fail-under=100 "--cov=${MAIN_MODULE}"
 done
