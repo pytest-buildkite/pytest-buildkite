662a89bb45b3f5ed63916a53107ddcd351a5b071
Tim Gates <tim.gates@iress.com>
2019-07-14 11:20:44 +1000

    Install spell checkers

--- app/install.sh
+++ app/install.sh
@@ -6,6 +6,10 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${BASEDIR}"
 
+apt-get update
+apt-get install -qq -y aspell aspell-en
+apt-get install -qq -y hunspell hunspell-en-au
+
 for PYVER in ${PYTHONVERS} ; do
   cd "${BASEDIR}/pip/${PYVER}"
   "python${PYVER}" -m pip install -r test_requirements.txt
