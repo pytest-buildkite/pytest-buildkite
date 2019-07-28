fc87e1674b6d0e68148b478ef701a96db7e46358
Tim Gates <tim.gates@iress.com>
2019-07-28 21:28:34 +1000

    Add entry point

--- app/install.sh
+++ app/install.sh
@@ -9,15 +9,17 @@ cd "${BASEDIR}"
 apt-get update
 apt-get install -qq -y aspell aspell-en
 apt-get install -qq -y hunspell hunspell-en-au
+apt-get install -qq -y shellcheck
 apt-get install -qq -y git
 
 for PYVER in ${PYTHONVERS} ; do
   cd "${BASEDIR}/pip/${PYVER}"
   for reqfile in */requirements.txt ; do
-    if [ $(wc -l < "${reqfile}") -gt 0 ] ; then
+    if [ "$(wc -l < "${reqfile}")" -gt 0 ] ; then
       "python${PYVER}" -m pip install -r "${reqfile}"
     fi
   done
   # Display installation
   "python${PYVER}" -m pip freeze
+  "python${PYVER}" -m safety check
 done
