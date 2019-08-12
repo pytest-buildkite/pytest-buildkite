a58a5f2032f10d2da9dd23a1ae9be5fe867f5c11
Tim Gates <tim.gates@iress.com>
2019-08-13 08:17:42 +1000

    Update post update hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,7 +6,15 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$(dirname "${BASEDIR}")"
 
 cd "${TOP}"
+rm \
+  app/pytest_buildkite/__main__.py \
+  app/tests/test_main.py
 git checkout -- \
  NEWS.rst \
  app/pytest_buildkite \
- app/tests
+ app/tests \
+ docs/modules.rst \
+ docs/setup.rst \
+ docs/tests.rst \
+ icon-above-font.png \
+ screenshot.png \
