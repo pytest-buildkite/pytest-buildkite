4e33a0431522ec3103d20502a699e095a6adea24
Tim Gates <tim.gates@iress.com>
2019-08-17 18:53:13 +1000

    Run black over files

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,4 +2,4 @@
 Pytest `conftest` plugin hook -
 https://docs.pytest.org/en/2.7.3/plugins.html
 """
-pytest_plugins = 'pytester'  # pylint: disable=invalid-name
+pytest_plugins = "pytester"  # pylint: disable=invalid-name
--- app/tests/test_buildkite.py
+++ app/tests/test_buildkite.py
@@ -8,20 +8,18 @@ def test_bar_fixture(testdir):
     """Make sure that pytest accepts our fixture."""
 
     # create a temporary pytest test module
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         def test_sth():
             assert 1 == 1
-    """)
-
-    # run pytest with the following command arguments
-    result = testdir.runpytest(
-        '-v'
+    """
     )
 
+    # run pytest with the following command arguments
+    result = testdir.runpytest("-v")
+
     # `fnmatch_lines` does an assertion internally
-    result.stdout.fnmatch_lines([
-        '*test_sth PASSED*',
-    ])
+    result.stdout.fnmatch_lines(["*test_sth PASSED*"])
 
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0  # nosec
@@ -32,22 +30,20 @@ def test_warning_output(testdir):
     Raise a warning during a test and check output
     """
     # create a temporary pytest test module
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import warnings
         def test_warnings():
             assert 1 == 1
             warnings.warn("Checking the warning feature inside a test")
-    """)
-
-    # run pytest with the following command arguments
-    result = testdir.runpytest(
-        '-v'
+    """
     )
 
+    # run pytest with the following command arguments
+    result = testdir.runpytest("-v")
+
     # `fnmatch_lines` does an assertion internally
-    result.stdout.fnmatch_lines([
-        '*test_warnings PASSED*',
-    ])
+    result.stdout.fnmatch_lines(["*test_warnings PASSED*"])
 
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0  # nosec
